9c329eac03cc9a9ab96dd8d69ee9f33420af7524
eltonsilvamtm <el.toon0811@gmail.com>
2021-05-02 17:10:31 +0000

    front end

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const bodyparser = require('body-parser');
+const bodyparser = require("body-parser");
 const path = require('path');
+
+const connectDB = require('./server/database/connection');
 
 const app = express();
 
-dotenv.config({path:'config.env'});
-const PORT = process.env.PORT||8080
+dotenv.config( { path : 'config.env'} )
+const PORT = process.env.PORT || 8080
 
-//log reuests
+// log requests
 app.use(morgan('tiny'));
 
-//parse request to body-parser
-app.use(bodyparser.urlencoded({extendd:true}))
+// mongodb connection
+connectDB();
 
-//set view engine
-app.set("view engine","ejs");
-//app.set("views",path.resolve(__dirname,"views/ejs"));
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended : true}))
 
-//load assets
-app.use('/css', express.static(path.resolve(__dirname,"assets/css"))); //to access a css stylesheet anywhere in the project you just need o write css/file.css
-app.use('/img', express.static(path.resolve(__dirname,"assets/img")));
-app.use('/js', express.static(path.resolve(__dirname,"assets/js")));
+// set view engine
+app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))
 
-app.get('/',(req, res)=>{
-    res.render('index');
-});
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-app.listen(PORT, ()=> {console.log(`Server is running on http://localhost:${PORT}`)});
+// load routers
+app.use('/', require('./server/routes/router'))
+
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
